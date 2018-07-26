209c97103592b053782814f15e9e3c938bde5554
unknown <uptimevic@gmail.com>
2018-07-26 09:24:14 -0400

    pre image

--- fullPageSite/assets/js/setup.js
+++ fullPageSite/assets/js/setup.js
@@ -1,57 +1,57 @@
 
 $(document).ready(function () {
-	// are we running in native app or in a browser?
-	window.isphone = false
-	if (document.URL.indexOf("http://") === -1
-		&& document.URL.indexOf("https://") === -1) {
-		window.isphone = true
-	}
+   // are we running in native app or in a browser?
+   window.isphone = false
+   if (document.URL.indexOf("http://") === -1
+      && document.URL.indexOf("https://") === -1) {
+      window.isphone = true
+   }
 
-	console.log('phonegap?', window.isphone)
-	if (window.isphone) { // //file is a browser
-		document.addEventListener("deviceready", onDeviceReady, false)
-	} else {
-		onDeviceReady()
-	}
+   console.log('phonegap?', window.isphone)
+   if (window.isphone) { // //file is a browser
+      document.addEventListener("deviceready", onDeviceReady, false)
+   } else {
+      onDeviceReady()
+   }
 })
 
 loadjs([
-	 //'https://cdn.jsdelivr.net/npm/signals@1.0.0/dist/signals.min.js'
-	//'//cdn.jsdelivr.net/npm/intersection-observer@0.5.0/intersection-observer.js'
-	  'https://unpkg.com/vivid-icons@1.0.3/dist/vivid-icons.min.js'
-	, 'https://unpkg.com/js-offcanvas/dist/_js/js-offcanvas.pkgd.min.js'
+    //'https://cdn.jsdelivr.net/npm/signals@1.0.0/dist/signals.min.js'
+   //'//cdn.jsdelivr.net/npm/intersection-observer@0.5.0/intersection-observer.js'
+     'https://unpkg.com/vivid-icons@1.0.3/dist/vivid-icons.min.js'
+   , 'https://unpkg.com/js-offcanvas/dist/_js/js-offcanvas.pkgd.min.js'
    , 'https://unpkg.com/js-offcanvas/dist/_css/prefixed/js-offcanvas.css'
-	, ROOT + '/assets/css/gridforms/gridforms.css'
-	, ROOT + '/assets/js/lorem.js'
+   , ROOT + '/assets/css/gridforms/gridforms.css'
+   , ROOT + '/assets/js/lorem.js'
 ], 'cssJs')
 
 function onDeviceReady() { // nothing will work before this
-	console.log('deviceready!')
-	loadjs.done('device')
+   console.log('deviceready!')
+   loadjs.done('device')
 }
 
 function cssLoaded() {// called by the style sheet in layout
-	loadjs.done('css')
+   loadjs.done('css')
 }
 
 loadjs.ready(['css', 'device', 'cssJs', 'fullPage'], function () {
-	loadjs.done('style')
+   loadjs.done('style')
 })
 
 // usage: ////////////////////////////////////////////////////////////////////
 loadjs.ready(['style'], function () {// 'show' page, ex: unhide
 
-	$('#off-canvas').offcanvas({
-		triggerButton: '#off-cbut' // btn to open offcanvas
-	})
-	let offcanvas = $('#off-canvas').data('offcanvas-component')
-	$('#offItems').click(function () {
-		console.log('#offItems')
-		offcanvas.close()
-	})
-	$('.delayShowing').removeClass('delayShowing') // show
+   $('#off-canvas').offcanvas({
+      triggerButton: '#off-cbut' // btn to open offcanvas
+   })
+   let offcanvas = $('#off-canvas').data('offcanvas-component')
+   $('#offItems').click(function () {
+      console.log('#offItems')
+      offcanvas.close()
+   })
+   $('.delayShowing').removeClass('delayShowing') // show
 
-	console.log('style done', Date.now() - _start)
+   console.log('style done', Date.now() - _start)
 })//ready
 
 //load full page:
@@ -62,14 +62,15 @@ loadjs([
 ], 'fullPage')
 
 loadjs.ready('fullPage', function () {
-	console.log('onFullPage')
-	$('#fullPage').fullpage({
-		scrollOverflow: true,
-		bigSectionsDestination: top,
-		pxaddingTop: '1.25em',
-		verticalCentered: false,
+   console.log('onFullPage')
+   $('#fullPage').fullpage({
+      scrollOverflow: true,
+      bxigSectionsDestination: top,
+      pxaddingTop: '1.25em',
+      verticalCentered: false,
 
-		css3: false,
-		lazyLoading: true
-	 })
+      css3: false,
+      loopBottom: true,
+      lazyLoading: true
+   })
 })//()
