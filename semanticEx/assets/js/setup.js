8884dc2168fdfd835938c7e74b09f89cedb52a03
unknown <uptimevic@gmail.com>
2018-08-05 12:55:09 -0400

    pre .8

--- semanticEx/assets/js/setup.js
+++ semanticEx/assets/js/setup.js
@@ -45,40 +45,40 @@ log.log('Hello, this is just a test')
 // add ie 11 fetch and promise as needed
 
 loadjs([
-	'//cdn.jsdelivr.net/npm/semantic-ui@2.3.1/dist/components/sidebar.min.js'
-	//,'//cdn.jsdelivr.net/npm/intersection-observer@0.5.0/intersection-observer.js'
+    '//cdn.jsdelivr.net/npm/semantic-ui@2.3.1/dist/components/sidebar.min.js'
+    //,
 ], 'cssJs')
-$( document ).ready(function() {
-	loadjs.done('site') // "done with bundle 'site'", need this because we're not loading js here
+$(document).ready(function () {
+    loadjs.done('site') // "done with bundle 'site'", need this because we're not loading js here
 })
 
 function cssLoaded() {// called by the style sheet in layout
-	console.log('css loaded', Date.now()-_start)
-	loadjs.done('css')
+    console.log('css loaded', Date.now() - _start)
+    loadjs.done('css')
 }
 
 loadjs.ready(['css', 'cssJs', 'site'], function () {
-	setTimeout(function(){
-		loadjs.done('style')
-	},1000/60)
+    setTimeout(function () {
+        loadjs.done('style')
+    }, 1000 / 60)
 })
 
 loadjs.ready(['style'], function () { //load large css
-	setTimeout(function(){
-		loadjs([ '/assets/css/semantic2.css'
-			,'//unpkg.com/vivid-icons@4.0.0/dist/css/vivid-icons.min.css' // http://vivid-icons.com/usage
-		], 'css2')
-	},1000/60)
+    setTimeout(function () {
+        loadjs(['/assets/css/semantic2.css'
+            , '//unpkg.com/vivid-icons@4.0.0/dist/css/vivid-icons.min.css' // http://vivid-icons.com/usage
+        ], 'css2')
+    }, 1000 / 60)
 })
 
 // util: /////////////////////////////////////////////////////////////////////
 function getUrlVars() {
-	var vars = [], hash
-	var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&')
-	for(var i = 0; i < hashes.length; i++) {
-		hash = hashes[i].split('=')
-		vars.push(hash[0])
-		vars[hash[0]] = hash[1]
-	}
-	return vars
+    var vars = [], hash
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&')
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=')
+        vars.push(hash[0])
+        vars[hash[0]] = hash[1]
+    }
+    return vars
 }
